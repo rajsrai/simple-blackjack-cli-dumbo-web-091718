af207f70946481af2a6a55824bd6f4f7d7032687
Rajvir Rai <rsr1992@gmail.com>
2018-09-09 15:25:15 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,9 +31,9 @@ end
 def hit?
   prompt_user
   get_user_input
-if input == 'h' || 's'
-    card_total + deal_card
-  elsif
+if get_user_input == 'h'
+    deal_card
+  elsif get_user_input == 's'
    card_total
     prompt_user
     get_user_input
